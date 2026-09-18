0b81db8dd7086669c420a829f42b0c06fd9a8b4a
Shruti Zade <shrutizade05@gmail.com>
2026-09-18 16:32:59 +0530

    continue project

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get("/hello",(req,res)=>{
 })
 
 app.get("/ig/:username", (req, res) => {
+    const followers = ["adam", "bob", "steve", "abc"];
     let { username } = req.params;
-    res.render("instagram.ejs" ,{username});
+    res.render("instagram.ejs" ,{username , followers});
 });
 
 app.get("/rolldice",(req,res) => {
